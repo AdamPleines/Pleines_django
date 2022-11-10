701e1138cbbc7bb9ae39593e66c83f23ec7501f5
Adam <adam.pleines@gmail.com>
2022-11-10 08:44:36 -0500

    inheritance and dark mode

--- django_project/main/urls.py
+++ django_project/main/urls.py
@@ -3,7 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:id>", views.index, name="index"),
     path("", views.home, name="home"),
+    path("<int:id>/", views.index, name="index"),
+    path("<int:id>", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("about", views.about, name="about"),
     path("v1/", views.v1, name="view 1"),
+    path("v1", views.v1, name="view 1"),
 ]
--- django_project/main/views.py
+++ django_project/main/views.py
@@ -11,5 +11,14 @@ def index(response, id):
 def home(response):
     return render(response, "main/home.html", {"name":"home"})
 
+def about(response, *args, **kwargs):
+    my_context = {
+        "name":"the about page",
+        "my_text": "This is my_text",
+        "my_number": 1234,
+        "my_list": [1313, 4231, 312, "Abcd"],
+    }
+    return render(response, "main/about.html", my_context)
+
 def v1(response):
     return HttpResponse("<h2>This is v1. I haven't really done anything here yet.</h2>")
